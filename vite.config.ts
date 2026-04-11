e5202d6b0c21f79e13f7b5bde4e6136fda7a27f4
Justin Wyne <1986068+wyne@users.noreply.github.com>
2026-04-11 00:19:55 -0400

    vite - local https for ws connections

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig(({ mode }) => {
   const basePath = mode === 'addon' ? './' : '/3Dash_webapp/';
@@ -9,6 +10,7 @@ export default defineConfig(({ mode }) => {
   base: basePath,
   plugins: [
     react(),
+    basicSsl(),
     VitePWA({
       registerType: 'autoUpdate',
       // Use the existing manifest.json in public/
@@ -58,6 +60,7 @@ export default defineConfig(({ mode }) => {
   },
   server: {
     host: true,
+    https: true,
   },
 };
 });
